ba66f04c82b8700cf394307ebc3f37f186e54334
Serges Pemba <spemba2@illinois.edu>
2019-08-07 01:48:34 -0400

    build14

--- chart_bar.js
+++ chart_bar.js
@@ -26,6 +26,38 @@ async function chartBar() {
                     .attr("height", height);
                 
         //We add our svg to the div area
+    //We add our svg to the div area
+    var tooltip = d3.select('#chartArea')
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .style("color", "white")
+
+    var formatTime = d3.timeFormat("%d-%b-%y");
+    var showTooltip = function(d) {
+    tooltip
+        .transition()
+        .duration(200)
+    tooltip
+        .style("opacity", 1)
+        .html(formatTime(d.Date) + '<br>' +"Total cases: " + d.cases )
+        .style("left", (d3.mouse(this)[0]+30) + "px")
+        .style("top", (d3.mouse(this)[1]+30) + "px")
+    }
+    var moveTooltip = function(d) {
+    tooltip
+        .style("left", (d3.mouse(this)[0]+30) + "px")
+        .style("top", (d3.mouse(this)[1]+30) + "px")
+    }
+    var hideTooltip = function(d) {
+    tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+    }
     svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
             .selectAll('rect').data(data).enter().append('rect')
@@ -33,15 +65,18 @@ async function chartBar() {
             .attr('y', function(d){return yscale(d.cases);})
             .attr('width', xscale.bandwidth())
             .attr('height', function(d){return height - margin.top - margin.bottom - yscale(d.cases);})
-            .attr('fill', "orange");
+            .attr('fill', "orange")
+            .on("mouseover", showTooltip )
+            .on("mousemove", moveTooltip )
+            .on("mouseleave", hideTooltip );
     svg.append('g')
             .attr('transform', 'translate('+ margin.left +','+ margin.top +')')
             .call(d3.axisLeft(yscale));
     svg.append('g')
           .attr('transform', 'translate('+margin.left+','+ (height + margin.top ) +')')
-          .call(d3.axisBottom(xscale))
-         // .tickFormat(d3.timeFormat("%d-%b-%y"))
-         // .tickValues(x.domain().filter(function(d,i){ return !(i%20)}));
+          .call(d3.axisBottom(xscale));
+          //.tickFormat(d3.timeFormat("%d-%b-%y"))
+          //.tickValues(xscale.domain().filter(function(d,i){ return !(i%20)}));
     svg.append("text")
           .attr("x", (width / 2))             
           .attr("y", height - (margin.bottom / 3))
